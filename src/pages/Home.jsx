2cf3a0bdf3a8ff8e163a01d9cddcafff532f9c95
Amal Alexander <59600587+amal-alexander@users.noreply.github.com>
2025-03-04 10:18:05 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import { useGetUsersQuery } from "../app/apiSlice";
 import Hero from "../components/Hero";
 import AboutMe from "../components/AboutMe";
 import Skills from "../components/Skills";
-import Projects from "../components/Projects";
+import Projects from "../components/projects";
 import Contact from "../components/Contact";
 import BackToTop from "../components/BackToTop";
 // Config
